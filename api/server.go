468bdf6e4e4b4b10993b0b8c1db8a669ae39f35b
Danyel Bayraktar <danyel@webhippie.de>
2015-04-05 22:55:00 +0200

    Implement click'n'load v2 listener. Move CLI code into cli package. Keep main file minimalistic.

--- api/server.go
+++ api/server.go
@@ -1,16 +1,29 @@
 package api
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/encoder"
+	"github.com/robertkrimen/otto"
 	"net/http"
+	"regexp"
 	"strconv"
 	"time"
 )
 
+var crossdomain = `<?xml version="1.0"?>
+<!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">
+<cross-domain-policy>
+<allow-access-from domain="*" />
+</cross-domain-policy>
+`
+
 type Server struct {
-	BindAddr  string    `json:"address,omitempty"`
+	BindAddr  string    `json:"bind_address,omitempty"`
 	Port      uint16    `json:"port"`
 	StartedAt time.Time `json:"started_at"`
 }
@@ -19,23 +32,61 @@ func (s *Server) Run() {
 	m := martini.New()
 	r := martini.NewRouter()
 	m.Action(r.Handle)
-	m.Use(jsonMiddleware)
-	r.Get("/serverinfo", encode(s))
+	m.Use(martini.Logger())
+	// API
+	r.Group("", func(r martini.Router) {
+		r.Get("/serverinfo", wrapEncode(s))
+	}, jsonMiddleware)
+	// CLICK'N'LOAD v2
+	r.Group("", func(r martini.Router) {
+		r.Post("/flash/addcrypted2", clickNLoad)
+		r.Get("/flash", wrap("UGET"))
+		r.Get("/jdcheck.js", as("text/javascript"), wrap("jdownloader = true;"))
+		r.Get("/crossdomain.xml", as("text/html"), wrap(crossdomain))
+	})
 	s.StartedAt = time.Now().Round(time.Minute)
-	m.RunOnAddr(s.BindAddr + ":" + fmt.Sprintf("%v", s.Port))
+	m.RunOnAddr(fmt.Sprintf("%v:%v", s.BindAddr, s.Port))
+}
+
+func clickNLoad(r *http.Request) (int, string) {
+	jk := r.FormValue("jk")
+	// pw := r.FormValue("pw")
+	crypted := r.FormValue("crypted")
+	vm := otto.New()
+	vm.Run(jk)
+	value, _ := vm.Run("f()")
+	key, _ := hex.DecodeString(value.String())
+	block, _ := aes.NewCipher(key)
+	data, _ := base64.StdEncoding.DecodeString(crypted)
+	c := cipher.NewCBCDecrypter(block, key)
+	c.CryptBlocks(data, data)
+	result := regexp.MustCompile(`\s+`).Split(string(data), -1)
+	fmt.Printf("Added new container with %v links.\n", len(result))
+	return 200, "success\r\n"
 }
 
 func jsonMiddleware(c martini.Context, w http.ResponseWriter, r *http.Request) {
 	pretty, _ := strconv.ParseBool(r.FormValue("pretty_json"))
-	ctype := r.Header.Get("Content-Type")
-	switch ctype {
-	default:
-		c.MapTo(encoder.JsonEncoder{PrettyPrint: pretty}, (*encoder.Encoder)(nil))
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	c.MapTo(encoder.JsonEncoder{PrettyPrint: pretty}, (*encoder.Encoder)(nil))
+	as("application/json")(w)
+}
+
+func as(ctype string) func(http.ResponseWriter) {
+	return func(w http.ResponseWriter) {
+		w.Header().Set("Content-Type", fmt.Sprintf("%s; charset=utf-8", ctype))
 	}
 }
 
-func encode(v interface{}) func(encoder.Encoder) []byte {
+func wrap(v string) func() string {
+	return func() string {
+		fmt.Print("EXIT")
+		return v
+	}
+}
+
+// Wraps a static value in a function block
+// This is a convenience method to use with martini
+func wrapEncode(v interface{}) func(encoder.Encoder) []byte {
 	return func(enc encoder.Encoder) []byte {
 		return encoder.Must(enc.Encode(v))
 	}
