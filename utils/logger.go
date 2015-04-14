7d6a4c2bcd2b2d03e294c62d4fa50254775b39f7
Danyel Bayraktar <danyel@webhippie.de>
2015-04-14 21:43:24 +0200

    Use file for logging.

--- utils/logger.go
+++ utils/logger.go
@@ -1,38 +1,26 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/cihub/seelog"
-	"os"
+	path "path/filepath"
+	"time"
 )
 
-var loggerConfig = `
+const loggerConfig = `
 <seelog>
   <outputs>
-    <custom name="myreceiver" formatid="testFormat"/>
+   <file path="%s" formatid="logformat" />
   </outputs>
   <formats>
-    <format id="testFormat" format="%EscM(49)%Date(02.01.2006 15:04:05.000) [%Level] %Msg%EscM(0)%n"/>
+    <format id="logformat" format="%%EscM(49)%%Date(02.01.2006 15:04:05.000) [%%Level] %%Msg%%EscM(0)%%n"/>
   </formats>
 </seelog>
 `
 
-type Receiver struct{}
-
-func (r Receiver) ReceiveMessage(message string, level seelog.LogLevel, context seelog.LogContextInterface) error {
-	os.Stderr.WriteString(message)
-	return nil
-}
-func (r Receiver) AfterParse(initArgs seelog.CustomReceiverInitArgs) error {
-	return nil
-}
-func (r Receiver) Flush() {}
-func (r Receiver) Close() error {
-	return nil
-}
-
 func InitLogger() {
-	seelog.RegisterReceiver("myreceiver", &Receiver{})
-	logger, err := seelog.LoggerFromConfigAsString(loggerConfig)
+	logfile := path.Join(app.UserLog(), time.Now().Local().Format("2006-01-02.log"))
+	logger, err := seelog.LoggerFromConfigAsString(fmt.Sprintf(loggerConfig, logfile))
 	if err != nil {
 		seelog.Error(err)
 	}
