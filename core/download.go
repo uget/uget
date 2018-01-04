aa97fb5a03171bdd32d09fcce3bcafdef8cf8721
Danyel Bayraktar <cydrop@gmail.com>
2018-01-04 20:03:12 +0800

    Download: Remove obsolete fields. Improve docs. Minor changes.

--- core/download.go
+++ core/download.go
@@ -3,78 +3,80 @@ package core
 import (
 	"io"
 	"os"
-	"sync"
 
 	"github.com/Sirupsen/logrus"
-	"github.com/chuckpreslar/emission"
 )
 
 // Download is an object that fetches a single remote file
+// and presents information on its progress and status
 type Download struct {
-	*emission.Emitter
 	Provider Provider
 	File     File
 	file     *os.File
 	reader   ReadProgress
-	err      error
-	errMtx   *sync.RWMutex
 	done     chan struct{}
+	err      error
 }
 
 // Done returns true if this download is finished. False otherwise
-func (g *Download) Done() bool {
+func (d *Download) Done() bool {
 	select {
-	case <-g.done:
+	case <-d.done:
 		return true
 	default:
 		return false
 	}
 }
 
+// Wait blocks the caller until this download is finished
+func (d *Download) Wait() {
+	<-d.done
+}
+
+// Waiter returns the channel which is closed on completion
+func (d *Download) Waiter() <-chan struct{} {
+	return d.done
+}
+
 // Download initalizes a Download object from the given File and ReadCloser
 func download(file File, reader ReadProgress) *Download {
 	return &Download{
-		Emitter: emission.NewEmitter(),
-		File:    file,
-		reader:  reader,
-		errMtx:  new(sync.RWMutex),
-		done:    make(chan struct{}),
+		File:   file,
+		reader: reader,
+		done:   make(chan struct{}),
 	}
 }
 
-// Progress returns the current progress in ints
-func (g *Download) Progress() int64 {
-	return g.reader.Progress()
+// Progress returns the current progress in int64
+func (d *Download) Progress() int64 {
+	return d.reader.Progress()
 }
 
-// Err returns the error during this download if there was one
-func (g *Download) Err() error {
-	g.errMtx.RLock()
-	defer g.errMtx.RUnlock()
-	return g.err
+// Err returns the error during this download if there was one.
+// This method panics if it is called before the download was finished.
+func (d *Download) Err() error {
+	if !d.Done() {
+		panic("Called Download#Err() before download was finished!")
+	}
+	return d.err
 }
 
-func (g *Download) to(file *os.File) *Download {
-	g.file = file
-	return g
+func (d *Download) to(file *os.File) *Download {
+	d.file = file
+	return d
 }
 
-func (g *Download) via(p Provider) *Download {
-	g.Provider = p
-	return g
+func (d *Download) via(p Provider) *Download {
+	d.Provider = p
+	return d
 }
 
-// Start reads the response body, copies its contents to the local file and emits events.
+// start reads the response body, copies its contents to the local file and emits events.
 // This will append to existing files. The caller needs to make sure the file does not exist!
-func (g *Download) start() {
-	defer close(g.done)
-	_, err := io.Copy(g.file, g.reader)
-	if err != nil {
-		g.errMtx.Lock()
-		defer g.errMtx.Unlock()
-		g.err = err
-	}
-	logrus.Debugf("Download#start: %v done, err: %v.", g.File.Name(), err)
+func (d *Download) start() {
+	defer close(d.done)
+	_, d.err = io.Copy(d.file, d.reader)
+	logrus.Debugf("Download#start: %v done, err: %v.", d.File.Name(), d.err)
 }
 
 // Progress is an object that represents a long operation that can track a progress
