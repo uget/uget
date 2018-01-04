c262a9dd2a5e5263de183611f8af22e77ed82874
Danyel Bayraktar <cydrop@gmail.com>
2018-01-04 19:58:40 +0800

    cli: Put more info in "done" status.

--- cli/commands.go
+++ cli/commands.go
@@ -171,7 +171,10 @@ func cmdGet(args []string, opts *options) int {
 					if err := inf.dl.Err(); err != nil {
 						con.EditRow(inf.row, fmt.Sprintf("%s: error: %v", inf.dl.File.Name(), err))
 					} else {
-						con.EditRow(inf.row, fmt.Sprintf("%s: done. Duration: %v", inf.dl.File.Name(), prettyTime(time.Since(inf.start))))
+						name := inf.dl.File.Name()
+						dl := units.BytesSize(float64(inf.dl.Progress()))
+						pt := prettyTime(time.Since(inf.start))
+						con.EditRow(inf.row, fmt.Sprintf("%s: downloaded %s in %s%s", name, dl, pt, inf.via))
 					}
 				} else {
 					con.EditRow(inf.row, fprog(inf.dl.File.Name(), float64(inf.prog), float64(inf.dl.File.Size()), float64(inf.rater.Rate()), inf.via))
