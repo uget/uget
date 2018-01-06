ffb4144a3046298fac4c2204d3e69f6efe5446d6
Danyel Bayraktar <cydrop@gmail.com>
2018-01-06 14:55:35 +0800

    Adjust prettyTime test to 365 days per year.

--- cli/helpers_test.go
+++ cli/helpers_test.go
@@ -13,7 +13,6 @@ func TestPrettyTime(t *testing.T) {
 	assert.Equal(t, "6h10m", prettyTime(6*time.Hour+10*time.Minute))
 	assert.Equal(t, "6h10m54s", prettyTime(6*time.Hour+10*time.Minute+54*time.Second))
 	assert.Equal(t, "9d6h10m54s", prettyTime(9*24*time.Hour+6*time.Hour+10*time.Minute+54*time.Second))
-	assert.Equal(t, "4y43m12s", prettyTime(4*365.25*24*time.Hour))
-	assert.Equal(t, "4y", prettyTime(126227808*time.Second))
-	assert.Equal(t, "55y10d", prettyTime(1736496360*time.Second))
+	assert.Equal(t, "4y", prettyTime(4*365*24*time.Hour))
+	assert.Equal(t, "55y10d", prettyTime(55*365*24*time.Hour+240*time.Hour))
 }
