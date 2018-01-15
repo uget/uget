1e448a2550c25f0f83de1c776ec3d44127c73e73
Danyel Bayraktar <cydrop@gmail.com>
2018-01-16 00:15:00 +0800

    core: remove extra client_*.go files and move into client.go.

--- core/client.go
+++ core/client.go
@@ -1,15 +1,21 @@
 package core
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
 	"reflect"
 	"strings"
 	"sync"
+	"sync/atomic"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/chuckpreslar/emission"
+	"github.com/uget/uget/core/api"
 )
 
 type event int
@@ -169,4 +175,276 @@ func (d *Client) OnDeadend(f func(*url.URL)) {
 
 func (d *Client) emit(e event, is ...interface{}) {
 	go d.emitter.EmitSync(e, is...)
+}
+
+// === RESOLVE METHODS ===
+
+// Resolve returns meta information on the given URLs
+func Resolve(urls []*url.URL) []File {
+	c := NewClient()
+	wg := c.AddURLs(urls)
+	c.Resolve()
+	wg.Wait()
+	c.Finalize()
+	files := make([]File, 0, len(urls))
+	for file := range c.ResolvedQueue.Dequeue() {
+		files = append(files, file)
+	}
+	return files
+}
+
+func (d *Client) workResolve() {
+	for jobs := range d.resolverQueue.getAll {
+		d.resolve(jobs)
+	}
+}
+
+func (d *Client) resolve(jobs []*request) {
+	units := d.units(jobs)
+	wg := new(sync.WaitGroup)
+	multis := make(chan *request)
+	wg.Add(len(units))
+	go func() {
+		reqs := make([]*request, 0, len(units))
+		for req := range multis {
+			reqs = append(reqs, req)
+		}
+		d.resolverQueue.enqueueAll(reqs)
+	}()
+	go func() {
+		wg.Wait()
+		close(multis)
+	}()
+	for _, unit := range units {
+		go func(unit resolveUnit) {
+			defer wg.Done()
+			requests := unit()
+			for _, req := range requests {
+				request := req.(*request)
+				if request.resolved() {
+					if request.file.Err() == nil && request.file.Offline() || d.retrievers == 0 {
+						request.done()
+					} else {
+						d.emit(eResolve, request.u, request.file, nil)
+					}
+					d.ResolvedQueue.enqueue(request)
+				} else {
+					_, ablty := d.resolvability(request)
+					if ablty == api.Single {
+						d.resolverQueue.enqueue(request)
+					} else {
+						multis <- request
+					}
+				}
+			}
+		}(unit)
+	}
+}
+
+type resolveUnit func() []api.Request
+
+// returns: units, retrievable (resolved)
+func (d *Client) units(requests []*request) []resolveUnit {
+	single, multi := d.group(requests)
+	fns := make([]resolveUnit, 0, len(single)+len(multi))
+	for req, resolver := range single {
+		request := req
+		fns = append(fns, func() []api.Request {
+			reqs, err := resolver.ResolveOne(request)
+			if err != nil {
+				if reqs != nil {
+					panic("non-nil request on err!")
+				}
+				reqs = req.resolvesTo(errored(req.root().u, req.u, err)).Wrap()
+			}
+			return reqs
+		})
+	}
+	for resolver, reqs := range multi {
+		rs := reqs
+		fns = append(fns, func() []api.Request {
+			reqs, err := resolver.ResolveMany(reqs)
+			if err != nil {
+				if reqs != nil {
+					panic("non-nil requests on err!")
+				}
+				reqs = make([]api.Request, len(rs))
+				for i, req := range rs {
+					local := req.(*request)
+					reqs[i] = local.resolvesTo(errored(local.root().u, local.u, err))
+				}
+			}
+			return reqs
+		})
+	}
+	return fns
+}
+
+// returns: (SingleResolvable, MultiResolvable)
+func (d *Client) group(rs []*request) (map[*request]SingleResolver, map[MultiResolver][]api.Request) {
+	single := make(map[*request]SingleResolver)
+	multi := make(map[MultiResolver][]api.Request)
+	for _, r := range rs {
+		if r.resolved() {
+			panic("Resolved in Client#group: " + r.URL().String())
+		}
+		resolver, ablty := d.resolvability(r)
+		if ablty == api.Single {
+			sr := resolver.(SingleResolver)
+			single[r] = sr
+		} else {
+			mr := resolver.(MultiResolver)
+			multi[mr] = append(multi[mr], r)
+		}
+	}
+	return single, multi
+}
+
+func (d *Client) resolvability(r *request) (resolver, api.Resolvability) {
+	for _, p := range d.Providers {
+		if resolver, ok := p.(resolver); ok {
+			switch resolver.CanResolve(r.URL()) {
+			case api.Single:
+				return resolver, api.Single
+			case api.Multi:
+				return resolver, api.Multi
+			}
+		}
+	}
+	panic("unreachable")
+}
+
+// === RETRIEVE METHODS ===
+
+func (d *Client) workRetrieve() {
+	for file := range d.ResolvedQueue.get {
+		if file.Err() != nil {
+			panic(fmt.Sprintf("File error in retrieve: %v", file.Err()))
+		} else if file.Offline() {
+			d.emit(eDeadend, file.URL())
+		} else {
+			d.download(file)
+			file.done()
+		}
+	}
+}
+
+func max(ps []Provider, f func(Provider) uint) Provider {
+	var max uint
+	var maxP Provider
+	for _, p := range ps {
+		prio := f(p)
+		if prio > max {
+			maxP = p
+			max = prio
+		}
+	}
+	return maxP
+}
+
+// Download retrieves the given File
+func (d *Client) download(file File) {
+	retriever := max(d.Providers, func(p Provider) uint {
+		if getter, ok := p.(Retriever); ok {
+			prio := getter.CanRetrieve(file)
+			logrus.Debugf("Client#download (%v): provider %v with prio %v", file.Name(), p.Name(), prio)
+			return prio
+		}
+		return 0
+	}).(Retriever)
+
+	path := filepath.Join(d.Directory, file.Name())
+	fi, err := os.Stat(path)
+	headers := map[string]string{}
+	if err == nil {
+		logrus.Debugf("Client#download (%v): local: %v, remote: %v", file.Name(), fi.Size(), file.Size())
+		if fi.Size() == file.Size() {
+			if !d.NoSkip {
+				logrus.Debugf("Client#download (%v): already exists... returning", file.Name())
+				d.emit(eSkip, file)
+				return
+			}
+			logrus.Debugf("Client#download (%v): already exists... deleting", file.Name())
+			err = os.Remove(path)
+			if err != nil {
+				d.emit(eError, file, err)
+				return
+			}
+		} else if !d.NoContinue {
+			headers["Range"] = fmt.Sprintf("bytes=%d-", fi.Size())
+			logrus.Infof("Client#download (%v): +header range %s", file.Name(), headers["Range"])
+		}
+	} else if !os.IsNotExist(err) {
+		d.emit(eError, 0, err)
+		return
+	}
+	if !d.dryRun("fetch %s with %s provider.", file.Name(), retriever.Name()) {
+		if req, err := retriever.Retrieve(file); err == nil {
+			for k, v := range headers {
+				req.Header.Set(k, v)
+			}
+			ctx, cancel := context.WithCancel(req.Context())
+			defer cancel()
+			req = req.WithContext(ctx)
+			resp, err := d.httpClient.Do(req)
+			if err != nil {
+				d.emit(eError, file, err)
+				return
+			}
+			defer resp.Body.Close()
+			logrus.Debugf("Client#download (%v): > %v", file.Name(), resp.Request.Header)
+			logrus.Debugf("Client#download (%v): %v", file.Name(), resp.Status)
+			for k, v := range resp.Header {
+				logrus.Debugf("  < %v: %v", k, v)
+			}
+			// Disallow redirects as well -- we haven't set a redirect handler
+			if !strings.HasPrefix(resp.Status, "2") {
+				logrus.Errorf("Client#download (%v): %v", file.Name(), resp.Status)
+				d.emit(eError, file, fmt.Errorf("status code %v", resp.Status))
+				return
+			}
+			reader := &passThru{Reader: resp.Body}
+			openFlags := os.O_WRONLY | os.O_CREATE
+			if resp.StatusCode == http.StatusPartialContent {
+				openFlags |= os.O_APPEND
+				reader.total = fi.Size()
+			} else if resp.StatusCode != http.StatusOK {
+				logrus.Warnf("Client#download (%v): unknown status code %v", file.Name(), resp.StatusCode)
+			}
+			f, err := os.OpenFile(path, openFlags, 0644)
+			if err != nil {
+				d.emit(eError, file, err)
+				return
+			}
+			download := download(file, reader).to(f).via(retriever)
+			download.cancel = cancel
+			d.emit(eDownload, download)
+			download.do()
+		} else {
+			d.emit(eError, file, err)
+		}
+	}
+	logrus.Debugf("Client#download (%v): EXIT", file.Name())
+}
+
+// PassThru wraps an existing io.Reader.
+//
+// It simply forwards the Read() call, while displaying
+// the results from individual calls to it.
+type passThru struct {
+	io.Reader
+	total int64 // Total # of bytes transferred
+}
+
+// Read 'overrides' the underlying io.Reader's Read method.
+// This is the one that will be called by io.Copy(). We simply
+// use it to keep track of byte counts and then forward the call.
+func (pt *passThru) Read(p []byte) (int, error) {
+	n, err := pt.Reader.Read(p)
+	atomic.AddInt64(&pt.total, int64(n))
+	return n, err
+}
+
+func (pt *passThru) Progress() int64 {
+	return atomic.LoadInt64(&pt.total)
 }
